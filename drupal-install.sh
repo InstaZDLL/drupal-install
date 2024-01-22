39158719e549445e65ce73585044c4105de14318
InstaZDLL <72951793+InstaZDLL@users.noreply.github.com>
2024-01-22 20:48:20 +0100

    Update drupal-install.sh

--- drupal-install.sh
+++ drupal-install.sh
@@ -118,6 +118,9 @@ sudo sed -i 's/;extension=pdo_mysql/extension=pdo_mysql/' /etc/php/8.2/cli/php.i
 sudo sed -i 's/;extension=gd/extension=gd/' /etc/php/8.2/apache2/php.ini
 sudo sed -i 's/;extension=pdo_mysql/extension=pdo_mysql/' /etc/php/8.2/apache2/php.ini
 
+# Get the public IP address
+public_ip=$(curl -s ifconfig.me)
+
 # Add the trusted_host_patterns setting to the settings.php file
 sudo sed -i "s/^# *\\\$settings\['trusted_host_patterns'\] = \[\];/\$settings['trusted_host_patterns'] = [\n    '^$public_ip$',\n    '^127\\.0\\.0\\.1$',\n    '^localhost$',\n];/" /var/www/html/drupal/sites/default/settings.php
 
@@ -139,5 +142,3 @@ echo "Database password: $password" >> /home/debian/db_credentials.txt
 echo "Database name: drupal"
 echo "Database user: drupal"
 echo "Database password: $password"
-
-
