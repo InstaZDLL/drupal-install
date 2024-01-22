aac8cc2b8353d23f928324bcbcd633928823405d
InstaZDLL <72951793+InstaZDLL@users.noreply.github.com>
2024-01-22 21:03:15 +0100

    Update trusted_host_patterns-fix.sh

--- trusted_host_patterns-fix.sh
+++ trusted_host_patterns-fix.sh
@@ -1,5 +1,14 @@
+#!/bin/bash
+#Début du scripte /  Made by © 2022-2023 Ethan Besson
+
+# Path to the settings.php file
+FILE="/var/www/html/drupal/sites/default/settings.php"
+
+# Backup the original file
+cp $FILE $FILE.bak
+
 # Get the public IP address
 public_ip=$(curl -s ifconfig.me)
 
-# Add the trusted_host_patterns setting to the settings.php file
-sudo sed -i "s/^# *\\\$settings\['trusted_host_patterns'\] = \[\];/\$settings['trusted_host_patterns'] = [\n    '^$public_ip$',\n    '^127\\.0\\.0\\.1$',\n    '^localhost$',\n];/" /var/www/html/drupal/sites/default/settings.php
+# Uncomment the line and change its value
+sed -i "s/# \$settings\['trusted_host_patterns'\] = \[\];/\$settings\['trusted_host_patterns'\] = ['^\\\$public_ip\\$', '^127\\\\.0\\\\.0\\\\.1\\$', '^localhost\\$',];/g" $FILE
