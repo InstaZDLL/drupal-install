d4f7a3f681d87b4bac67174a581ec7c9b1270845
InstaZDLL <72951793+InstaZDLL@users.noreply.github.com>
2024-02-19 22:30:09 +0100

    Changing the path of the setting.php

--- drupal-install-composer.sh
+++ drupal-install-composer.sh
@@ -121,7 +121,7 @@ sudo sed -i 's/;extension=pdo_mysql/extension=pdo_mysql/' /etc/php/8.2/apache2/p
 public_ip=$(curl -s ifconfig.me)
 
 # Append the new line after the trusted_host_patterns line in the settings.php file
-sudo sed -i "/# *\$settings\['trusted_host_patterns'\] = \[\];/a \$settings['trusted_host_patterns'] = ['^${public_ip//./\\.}$', '^127\\.0\\.0\\.1$', '^localhost$',];" /var/www/html/drupal/sites/default/settings.php
+sudo sed -i "/# *\$settings\['trusted_host_patterns'\] = \[\];/a \$settings['trusted_host_patterns'] = ['^${public_ip//./\\.}$', '^127\\.0\\.0\\.1$', '^localhost$',];" /var/www/html/drupal/web/sites/default/settings.php
 
 # Restart Apache
 sudo systemctl restart apache2
